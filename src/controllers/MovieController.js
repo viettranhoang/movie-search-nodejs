981705ba886111ffd4b92d469ee3175725d0c4f3
vit <anhxtanhwp@gmail.com>
2021-01-16 17:31:38 +0700

    update

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -46,43 +46,7 @@ class MovieController {
             console.log(error);
             res.send(error)
         })
-       
-        // request(movieLink, (error, response, html) => {
-  
-        //     if(!error && response.statusCode == 200) {
-        //         const $ = cheerio.load(html); 
-            
-        //         const name =  $('.movie-title').find('.title-1 a').text()
-        //         const globalName =  $('.movie-title').find('.title-2').text()
-        //         const year =  $('.movie-title').find('.title-year').text()
-        //         const imdb =  $('dd.movie-dd.imdb').text()
-        //         const numberOfVotes =  $('dt.movie-dt.hidden').nextUntil('dt.movie-dt').text();
-        //         const country = 
-        //             $('.country')
-        //                 .map(function (i, el) {
-        //                     // this === el
-        //                     return $(this).text();
-        //                 })
-        //                 .get()
-        //                 .join(', ');
-        //         const content =  $('.content').find('p').text()
-        //         const poster =  $('.movie-l-img').find('img').attr('src')
-        //         const trailerUrl = $('.btn-film-trailer').attr('data-videourl')
-        //         const facebookLink = $("div.fb-comments").attr('data-href')
-        //         var fbCommentUrl = fbPageCommentUrl + encodeURI(facebookLink);
-            
-            
-        //         var movie = {
-        //             name, globalName, year, imdb, numberOfVotes, content, country, poster, trailerUrl, facebookLink, fbCommentUrl
-        //         }
-
-        //         res.json(movie)
-        //     }
-        //     else {
-        //         console.log(error);
-        //         res.send(error)
-        //     }
-        // });
+    
     }
 }
 
